20af1d52174ffe7dc95d31f9b5fb650a7490a8e0
FelixErnst <felix.gm.ernst@outlook.com>
2020-02-04 23:26:09 +0100

    added modifications() function

---
+++ R/AllGenerics.R
@@ -0,0 +1,9 @@
+
+setGeneric("modifications",
+           function(x, ...) standardGeneric("modifications"))
+
+setGeneric("modificationsByTranscript",
+           function(x, ...) standardGeneric("modificationsByTranscript"))
+
+setGeneric("modifiedSeqsByTranscript",
+           function(x, ...) standardGeneric("modifiedSeqsByTranscript"))
---
+++ R/modifications.R
@@ -0,0 +1,159 @@
+#' @include TxModDb-class.R
+NULL
+
+#' @name modifications
+#' @aliases modifications modificationsBy
+#'
+#' @title modifications
+#'
+#' @description
+#' title
+#'
+#' @param x a \code{\link[=TxModDb-class]{TxModDb}}
+#' @param columns XXX
+#' @param filter XXX
+#' @use.names XXX
+#'
+#' @examples
+#' \dontrun{
+#' library(TxModDb.Hsapiens.hg38)
+#' modifications(TxModDb.Hsapiens.hg38))
+#' }
+NULL
+
+# helper functions for extracting feature data ---------------------------------
+
+.extract_features <- function(txmoddb, table, mcolumns = character(0),
+                              filter = list(), core_columns){
+    schema_version <- TxModDb_schema_version(txmoddb)
+    names(mcolumns) <- TXMODDB_column2table(mcolumns, from_table = table,
+                                            schema_version = schema_version)
+    proxy_column <- orderby <- c(modification = "_mod_id")
+
+    ## 1st SELECT: extract stuff from the proxy table.
+    columns1 <- union(core_columns, mcolumns[names(mcolumns) == table])
+    df1 <- TxModDb_SELECT_from_INNER_JOIN(txmoddb, table, columns1,
+                                          filter = filter, orderby = orderby)
+
+    ## Additional SELECTs: 1 additional SELECT per satellite table
+    foreign_columns <- mcolumns[names(mcolumns) != table]
+    foreign_columns <- split(foreign_columns, names(foreign_columns))
+    satellite_tables <- names(foreign_columns)
+    names(satellite_tables) <- satellite_tables
+    df_list <- lapply(satellite_tables, function(satellite_table) {
+        columns2 <- foreign_columns[[satellite_table]]
+        if (length(filter) == 0L) {
+            filter2 <- list()
+        } else {
+            filter2 <- list(df1[[proxy_column]])
+            names(filter2) <- proxy_column
+        }
+        columns2 <- c(proxy_column, columns2)
+        orderby <- c("_mod_id")
+        TxModDb_SELECT_from_INNER_JOIN(txmoddb, satellite_table, columns2,
+                                       filter = filter2, orderby = orderby)
+    })
+    df1 <- list(df1)
+    names(df1) <- table
+    c(df1, df_list)
+}
+
+
+# .extract_features_as_GRanges() -----------------------------------------------
+
+.make_DataFrame_from_df_list <- GenomicFeatures:::.make_DataFrame_from_df_list
+.assignMetadataList <- GenomicFeatures:::.assignMetadataList
+
+.as_db_columns <- function(columns)
+    sub("^(mod_id)$", "_\\1", columns)
+
+.extract_features_as_GRanges <- function(txmoddb, table,
+                                         mcolumns = character(0),
+                                         filter = list(), use.names = FALSE) {
+    if (!isTRUEorFALSE(use.names))
+        stop("'use.names' must be TRUE or FALSE")
+    db_mcolumns <- db_mcolumns0 <- .as_db_columns(mcolumns)
+    proxy_columns <- TXMODDB_table_columns(table)
+    if (use.names) {
+        if ("name" %in% names(proxy_columns)) {
+            proxy_name_column <- proxy_columns[["name"]]
+            if (!(proxy_name_column %in% db_mcolumns0))
+                db_mcolumns <- c(db_mcolumns0, proxy_name_column)
+        } else {
+            warning("no column in '", table, "' table to retrieve the feature ",
+                    "names from")
+            use.names <- FALSE
+        }
+    }
+    names(filter) <- .as_db_columns(names(filter))
+    core_columns <- proxy_columns[proxy_columns %in% TXMODDB_MOD_COLUMNS]
+    df_list <- .extract_features(txmoddb, table, db_mcolumns,
+                                 filter, core_columns)
+    DF <- .make_DataFrame_from_df_list(df_list)
+    DF$seqnames <- .get_seqnames(DF)
+    ans <- GenomicRanges::makeGRangesFromDataFrame(
+        DF,
+        seqinfo = .get_TxModDb_seqinfo(txmoddb),
+        seqnames.field = "seqnames",
+        start.field = "mod_start",
+        end.field = "mod_end")
+    if (use.names) {
+        ans_names <- DF[ , proxy_name_column]
+        ans_names[is.na(ans_names)] <- ""  # replace NAs with empty strings
+        names(ans) <- ans_names
+    }
+    mcols(ans) <- setNames(DF[db_mcolumns0], mcolumns)
+    ans
+}
+
+# translate external to internal column names
+translateCols <- function(columns, txmoddb){
+    ## preserve any names
+    oriColNames <- names(columns)
+    ## and save the original column strings
+    oriCols <- columns
+
+    oriLen <- length(columns) ## not always the same as length(oriCols)
+    ## get the available abbreviations as a translation vector (exp)
+    names <- .makeColAbbreviations(txmoddb)
+    exp <- sub("^_","", names(names))
+    names(exp) <- names
+
+    ## Then replace only those IDs that match the UC names
+    m <- match(oriCols, names(exp))
+    idx = which(!is.na(m))
+    columns[idx] <- exp[m[idx]]
+
+    if(length(columns) == oriLen && is.null(oriColNames)) {
+        names(columns) <- oriCols
+    } else if(length(columns) == oriLen && !is.null(oriColNames)) {
+        names(columns) <- oriColNames
+    } else if(length(columns) != oriLen) {
+        stop("names were lost in translateCols() helper")
+    }
+    columns
+}
+
+.extractFromTxModDb <- function(txmoddb, table, mcolumns = NULL,
+                                filter = NULL, use.names = FALSE){
+    user_mcolumns <- mcolumns
+    mcolumns <- translateCols(mcolumns, txmoddb)
+    if (is.null(filter))
+        filter <- list()
+    names(filter) <- translateCols(names(filter), txmoddb)
+    ans <- .extract_features_as_GRanges(txmoddb, table, mcolumns, filter,
+                                        use.names)
+    names(mcols(ans)) <- if (is.null(names(user_mcolumns))) user_mcolumns
+    else names(user_mcolumns)
+    .assignMetadataList(ans, txmoddb)
+}
+
+#' @rdname modifications
+#' @export
+setMethod("modifications", "TxModDb",
+    function(x, column = c("mod_id","mod_type","mod_name"),
+           filter = NULL, use.names = FALSE){
+      .extractFromTxModDb(x, "modification", mcolumns = column,
+                          filter = filter, use.names = use.names)
+    }
+)
