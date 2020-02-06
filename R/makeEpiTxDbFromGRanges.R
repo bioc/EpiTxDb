ff9fc64538ec0ff304386a8ba824e709a993a17c
FelixErnst <felix.gm.ernst@outlook.com>
2020-02-07 00:39:14 +0100

    refactored makeEpiTxDbfromGRanges()

--- R/makeEpiTxDbFromGRanges.R
+++ R/makeEpiTxDbFromGRanges.R
@@ -14,10 +14,8 @@ NULL
 
 # makeEpiTxDbfromGRanges -------------------------------------------------------
 
-#' @rdname makeEpiTxDbfromGRanges
-#' @export
-makeEpiTxDbfromGRanges <- function(gr, metadata = NULL, reassign.ids = FALSE){
-    # extract modification table
+# extract modification table
+.extract_modifications <- function(gr){
     modifications <- list(mod_id = mcols(gr)$mod_id,
                           mod_type = mcols(gr)$mod_type,
                           mod_name = mcols(gr)$mod_name,
@@ -34,7 +32,11 @@ makeEpiTxDbfromGRanges <- function(gr, metadata = NULL, reassign.ids = FALSE){
         stop("Couldn't extranct modification information from 'GRanges' object.",
              call. = FALSE)
     }
-    # extract reactions table
+    modifications
+}
+
+# extract reactions table
+.extract_reactions <- function(gr){
     reactions <- list(mod_id = mcols(gr)$mod_id,
                       mod_rank = mcols(gr)$mod_rank,
                       reaction_genename = mcols(gr)$reaction_genename,
@@ -49,20 +51,28 @@ makeEpiTxDbfromGRanges <- function(gr, metadata = NULL, reassign.ids = FALSE){
     } else {
         reactions <- NULL
     }
-    # extract specifiers table
+    reactions
+}
+
+# extract specifiers table
+.extract_specifiers <- function(gr){
     specifiers <- list(mod_id = mcols(gr)$mod_id,
-                      specifier_type = mcols(gr)$specifier_type,
-                      specifier_genename = mcols(gr)$specifier_genename,
-                      specifier_entrezid = mcols(gr)$reaction_entrezid,
-                      specifier_ensembl = mcols(gr)$specifier_ensembl)
+                       specifier_type = mcols(gr)$specifier_type,
+                       specifier_genename = mcols(gr)$specifier_genename,
+                       specifier_entrezid = mcols(gr)$reaction_entrezid,
+                       specifier_ensembl = mcols(gr)$specifier_ensembl)
     specifiers <- specifiers[!vapply(specifiers, is.null, logical(1))]
     if(length(specifiers) > 1L){
         specifiers <- data.frame(specifiers, check.names = FALSE,
-                                stringsAsFactors = FALSE)
+                                 stringsAsFactors = FALSE)
     } else {
         specifiers <- NULL
     }
-    # extract references table
+    specifiers
+}
+
+# extract references table
+.extract_references <- function(gr){
     ref_lengths <- lengths(mcols(gr)$reference_type)
     references <- list(mod_id = unlist(Map(rep,mcols(gr)$mod_id,ref_lengths)),
                        reference_type = unlist(mcols(gr)$reference_type),
@@ -74,7 +84,17 @@ makeEpiTxDbfromGRanges <- function(gr, metadata = NULL, reassign.ids = FALSE){
     } else {
         references <- NULL
     }
-    makeEpiTxDb(modifications, reactions = reactions, specifiers = specifiers,
-                references = references, metadata = metadata,
-                reassign.ids = reassign.ids)
+    references
+}
+
+#' @rdname makeEpiTxDbfromGRanges
+#' @export
+makeEpiTxDbfromGRanges <- function(gr, metadata = NULL, reassign.ids = FALSE){
+    modifications <- .extract_modifications(gr)
+    reactions <- .extract_reactions(gr)
+    specifiers <- .extract_specifiers(gr)
+    references <- .extract_references(gr)
+    makeEpiTxDb(modifications = modifications, reactions = reactions,
+                specifiers = specifiers, references = references,
+                metadata = metadata, reassign.ids = reassign.ids)
 }
